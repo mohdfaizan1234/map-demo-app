17f44c18e946f10fcaaba3247cc3408a164b4161
Mohd Faizan <mohd.faizan1@fluper.in>
2026-06-21 00:55:04 +0530

    Initial commit

--- MapDemoApp/ViewController.swift
+++ MapDemoApp/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+     let mapView = MKMapView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView.frame = view.bounds
+        view.addSubview(mapView)
+       
+        let location = CLLocationCoordinate2D(latitude:28.6395, longitude
+                                              :78.6427)
+        let region = MKCoordinateRegion(center:location, latitudinalMeters:1000, longitudinalMeters:1000)
+        
+        mapView.setRegion(region, animated:true)
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = location
+        pin.title = "Sirsi sambhal"
+        
+        mapView.addAnnotation(pin)
+        
     }
-
-
 }
 
